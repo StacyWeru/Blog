541af070dc77a25e1431f2990b5f083e5409de84
StacyWeru <stacyweru@gmail.com>
2020-11-02 22:42:26 +0300

    Reistered the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,30 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-#Initializing the application
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
-#Setting up the configurations
-app.config.from_object(DevConfig)
-
-#Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
 
 
-from app import views
-from app import error
+    #Initializing the application
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options(config_name))
+
+    # #Setting up the configurations
+    # app.config.from_object(DevConfig)
+
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    #Registering the Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    from app import views
+    from app import error
+
+    return app
